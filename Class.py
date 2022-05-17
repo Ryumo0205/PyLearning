12fd2b3477f2b4a5cf64a0c24c49197bb7942b0b
Sigen_Chang <97452067+Ryumo0205@users.noreply.github.com>
2022-05-17 15:03:28 +0800

    0517

--- Class.py
+++ Class.py
@@ -2,6 +2,9 @@
 
 #定義類別與類別屬性(類別屬性是封裝在類別中的變數與函式)
 
+from tkinter import Y
+
+
 class IO :
     X_list = ["console","File"]
     def read(src):
@@ -12,4 +15,10 @@ IO.read("File")
 
 
 #類別與實體物件,實體屬性
-#git3
+class CLASS_A:
+    def __init__(self,x,y) -> None:
+        self.x=x
+        self.y=y
+p1=CLASS_A(1,2)
+p2=CLASS_A(3,4)
+print(p1.x+p1.y,p2.x+p2.y)
